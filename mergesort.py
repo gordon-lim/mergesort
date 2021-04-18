daf362064b9271a074e9f5bb5d1a9cfa0e5c39c7
bongbonglemon <gordonlim214@gmail.com>
2021-04-18 22:40:34 +0800

    initial commit

---
+++ mergesort.py
@@ -0,0 +1,30 @@
+def mergesort(array):
+    n = len(array)
+    if(n==1):
+        return array
+    m = n//2 if (n%2==0) else n//2+1
+    l = array[:m]
+    r = array[m:]
+    l_prime = mergesort(l)
+    r_prime = mergesort(r)
+    #merge
+    new_arr=[]
+    left_finger = 0
+    right_finger = 0
+
+    while True:
+        if (left_finger>len(l_prime)-1):
+            new_arr.extend(r_prime[right_finger:])
+            break
+        elif(right_finger>len(r_prime)-1):
+            new_arr.extend(l_prime[left_finger:])
+            break
+        if(l_prime[left_finger]<r_prime[right_finger]):
+            new_arr.append(l_prime[left_finger])
+            left_finger = left_finger + 1
+        else:
+            new_arr.append(r_prime[right_finger])
+            right_finger = right_finger + 1
+    return new_arr
+
+print(mergesort([3,1,2,4,5,7,9,8,2,4,3,100,50]))
